5de23c88e5cbebeabd5dd29d70218d6155881603
Spop08 <future826@hotmail.com>
2019-12-16 12:35:40 +0100

    import libraries

---
+++ src/components/BridePage/FeedPhoto.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import Card from '@material-ui/core/Card';
+import Button from '@material-ui/core/Button';
+import { withRouter } from 'react-router';
+import { portfolioService } from '../../_services';
+  
