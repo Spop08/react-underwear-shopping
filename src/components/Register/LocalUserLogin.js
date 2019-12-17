d31e690fca1d90ad69edf17b94be1b1917ad2389
Spop08 <future826@hotmail.com>
2019-12-17 11:00:43 +0100

    import libraries

---
+++ src/components/Register/LocalUserLogin.js
@@ -0,0 +1,8 @@
+import React, {Component} from 'react';
+import {withStyles} from '@material-ui/core/styles';
+import Formsy from 'formsy-react';
+import Button from '@material-ui/core/Button';
+import PropTypes from 'prop-types';
+
+import ValidatedTextField from './components/ValidatedTextField';
+import LoginRegisterError from "./components/LoginRegisterError";
