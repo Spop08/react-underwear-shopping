049181f5ce33f4ca1e3e3b5400a2ffd773d9dd24
Spop08 <future826@hotmail.com>
2019-12-18 11:27:23 +0100

    githubbox and twitterbox added

---
+++ src/components/Signin/components/ProviderButtons.js
@@ -0,0 +1,6 @@
+import React, {Component} from 'react';
+import {withStyles} from '@material-ui/core/styles';
+import {darken} from '@material-ui/core/styles/colorManipulator'
+import Button from "@material-ui/core/Button";
+import PropTypes from 'prop-types';
+import {FacebookBox, GithubBox, TwitterBox, Google} from "mdi-material-ui";
