d7cfb13d1b59478c6dcfbe605323dac56285ed8e
Spop08 <future826@hotmail.com>
2019-12-18 11:42:50 +0100

    formsy-react

---
+++ src/components/Signin/components/ValidatedTextField.js
@@ -0,0 +1,4 @@
+import React, {Component} from 'react';
+import TextField from '@material-ui/core/TextField';
+import {withFormsy, propTypes as formsyPropTypes} from 'formsy-react';
+import PropTypes from 'prop-types'
