0c586e9941578a4712bcf32096ece698f5c1bd28
Spop08 <future826@hotmail.com>
2019-12-18 11:43:08 +0100

    create propTypes

--- src/components/Signin/components/ValidatedTextField.js
+++ src/components/Signin/components/ValidatedTextField.js
@@ -2,3 +2,14 @@ import React, {Component} from 'react';
 import TextField from '@material-ui/core/TextField';
 import {withFormsy, propTypes as formsyPropTypes} from 'formsy-react';
 import PropTypes from 'prop-types'
+
+class ValidatedTextField extends Component {
+
+  static propTypes = {
+    ...formsyPropTypes,
+    type: PropTypes.string,
+    name: PropTypes.string,
+    label: PropTypes.string,
+    className: PropTypes.string,
+    autoComplete: PropTypes.string
+  };
