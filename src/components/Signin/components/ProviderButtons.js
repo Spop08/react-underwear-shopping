bf66ebef39a06848f44af0c3d3b25b056bf87ea3
Spop08 <future826@hotmail.com>
2019-12-18 11:27:44 +0100

    var providerColors created

--- src/components/Signin/components/ProviderButtons.js
+++ src/components/Signin/components/ProviderButtons.js
@@ -4,3 +4,18 @@ import {darken} from '@material-ui/core/styles/colorManipulator'
 import Button from "@material-ui/core/Button";
 import PropTypes from 'prop-types';
 import {FacebookBox, GithubBox, TwitterBox, Google} from "mdi-material-ui";
+
+const providerColors = {
+  github: {
+    primary: '#24292e'
+  },
+  facebook: {
+    primary: '#3b5998'
+  },
+  twitter: {
+    primary: '#1da1f2'
+  },
+  google: {
+    primary: '#4285F4'
+  }
+};
