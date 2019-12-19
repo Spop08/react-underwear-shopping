306c9f5a69674e2ad3223712993afa7384c1b517
Spop08 <future826@hotmail.com>
2019-12-19 12:01:20 +0100

    add sign up button

--- src/components/Signup/index.js
+++ src/components/Signup/index.js
@@ -149,3 +149,25 @@ render() {
                </div>
                </div>)}      
         </div>
+        {this.state.error.password?<ul><li style={{textAlign:'center', color:'red', margin:'0px', padding:'0px', textAlign:'left'}}>{this.state.error.password[0]}</li></ul>:''} 
+            <Button className="btn btn-lg btn-primary" type="submit">
+              Sign Up
+            </Button>
+          </Form>
+          <svg
+            className="rocks"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 100 100"
+            preserveAspectRatio="none"
+          >
+            <polygon
+              className="svg--sm"
+              fill="red"
+              points="0,0 30,100 65,21 90,100 100,75 100,100 0,100"
+            />
+            <polygon
+              className="svg--lg"
+              fill="red"
+              points="0,0 15,100 33,21 45,100 50,75 55,100 72,20 85,100 95,50 100,80 100,100 0,100"
+            />
+          </svg>
