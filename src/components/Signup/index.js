a63c76050f402b65b7115f1af1e4a4fac4353433
Spop08 <future826@hotmail.com>
2019-12-19 11:56:46 +0100

    import Api and Header

---
+++ src/components/Signup/index.js
@@ -0,0 +1,6 @@
+import React, { Component } from "react";
+import superagent from "superagent";
+import ArrowData from "../../Shared/ArrowDataComponent.jsx";
+import { LogInCard, Form, Button, H3, H4 } from "../../Shared/styles.js";
+import APIPath from '../Api';
+import Header from '../Header'
