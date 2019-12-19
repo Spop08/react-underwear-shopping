b035efd1d1a4172ec2d3ce60ba8e7a01fe10efc8
Spop08 <future826@hotmail.com>
2019-12-19 11:57:10 +0100

    define invite_token and other states

--- src/components/Signup/index.js
+++ src/components/Signup/index.js
@@ -4,3 +4,16 @@ import ArrowData from "../../Shared/ArrowDataComponent.jsx";
 import { LogInCard, Form, Button, H3, H4 } from "../../Shared/styles.js";
 import APIPath from '../Api';
 import Header from '../Header'
+
+class Signup extends Component {
+  constructor() {
+  super();
+  this.state = {
+      invite_token: "",
+      password: "",
+      username: "",
+      email: "",
+      showPassword: false,
+      error: new Object()
+  };
+}
