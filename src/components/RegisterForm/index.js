2ce0f71227c58573b311e58c8d28f004d02fbe0b
Spop08 <future826@hotmail.com>
2019-12-20 12:19:05 +0100

    import API and actions

---
+++ src/components/RegisterForm/index.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react'
+import {render} from 'react-dom'
+import { Link, Redirect } from 'react-router-dom';
+import 'typeface-roboto';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import {AppBar, Toolbar, Typography} from '@material-ui/core';
+import {withStyles} from '@material-ui/core/styles';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Paper from '@material-ui/core/Paper';
+import LoginRegister from '../Register';
+import Button from '@material-ui/core/Button'
+import superagent from "superagent";
+import APIPath from '../Api';
+import { accountActions } from '../../_actions';
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router';
