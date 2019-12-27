4cbac8b269022077d0df4b0e7ae161b906214f75
Spop08 <future826@hotmail.com>
2019-12-27 12:36:45 +0100

    FeedPhoto constructor

--- src/components/BridePage/FeedPhoto.js
+++ src/components/BridePage/FeedPhoto.js
@@ -3,4 +3,20 @@ import Card from '@material-ui/core/Card';
 import Button from '@material-ui/core/Button';
 import { withRouter } from 'react-router';
 import { portfolioService } from '../../_services';
-  
+
+class FeedPhoto extends React.Component{
+    
+    constructor(props) {
+        super(props)
+        this.state = {
+            hover: false
+        }
+        this.toggleHover = this.toggleHover.bind(this);
+        this.lolChange = this.lolChange.bind(this);
+        this.onDetail = this.onDetail.bind(this);
+    }
+    componentDidMount(){
+        this.setState({lol:this.props.info.lol})
+    }
+
+}
