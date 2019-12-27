7d020ef9142c9434ad774e2ca139310e925ea95b
Spop08 <future826@hotmail.com>
2019-12-27 12:37:07 +0100

    lolChange function created

--- src/components/BridePage/FeedPhoto.js
+++ src/components/BridePage/FeedPhoto.js
@@ -19,4 +19,27 @@ class FeedPhoto extends React.Component{
         this.setState({lol:this.props.info.lol})
     }
 
+    lolChange(state) {
+        console.log(this.props.info.uniq_id);
+        if(this.state.lol === 2 && (state === 0 || state === 1))
+        {
+            portfolioService
+            .removeGift({uniq_id: this.props.info.uniq_id})
+            .then((response) => {
+              console.log("REMOVE GIFT RESPONSE:", response);
+            })     
+        }
+        if(state === 2)
+        {
+            portfolioService
+            .addGift({uniq_id: this.props.info.uniq_id})
+            .then((response) => {
+              console.log("ADD GIFT RESPONSE:", response);
+            })
+        }
+        this.setState({lol: state});
+        this.props.onChangeLOL(this.props.photo_ind, state);
+        portfolioService.changeLOL({uniq_id:this.props.info.uniq_id, lol:state})
+        
+    }
 }
